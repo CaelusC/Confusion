0cb622fe1ab132639a7a2747827bee6d8804b4bf
Caelus <562505@student.saxion.nl>
2026-03-24 11:41:40 +0100

    fix: minor syntax error fixed

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,8 +1,8 @@
 class Project < ApplicationRecord
-  has many :memberships, dependent: :destroy
-  has many :users, through: :memberships
-  has many :sprints, dependent: :destroy
-  has many :cards, dependent: :destroy
-  has many :folders, dependent: :destroy
-  has many :tags, dependent: :destroy
+  has_many :memberships, dependent: :destroy
+  has_many :users, through: :memberships
+  has_many :sprints, dependent: :destroy
+  has_many :cards, dependent: :destroy
+  has_many :folders, dependent: :destroy
+  has_many :tags, dependent: :destroy
 end
